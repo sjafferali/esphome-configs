e2cd444fa984ab9e38be3511fa9ff91dd31cc82c
Samir Jafferali <sjafferali@netflix.com>
2022-03-06 12:35:13 -0800

    Remove esphome directory on every run

--- dashboard.sh
+++ dashboard.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+find . -maxdepth 2 -mindepth 1 -type d -name .esphome | while read line ; do
+    echo "Removing $line"
+    rm -fr $line
+done
+
+
 ESPHOME="/Users/sjafferali/.virtualenvs/esphome/bin/esphome"
 PATH="/Users/sjafferali/.virtualenvs/esphome/bin:$PATH" $ESPHOME dashboard config/
